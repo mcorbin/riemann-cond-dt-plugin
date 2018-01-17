0678b59216b4cd920ee3571dc27dd02c9a258966
mcorbin <corbin.math@gmail.com>
2018-01-17 22:32:55 +0100

    1.0.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject riemann-cond-dt "1.0.3"
+(defproject riemann-cond-dt "1.0.4-SNAPSHOT"
   :description "Riemann plugin for checking if something is true for a time period. "
   :url "https://github.com/mcorbin/riemann-cond-dt-plugin"
   :license {:name "Eclipse Public License"
